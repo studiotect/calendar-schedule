112c7d0ebbcc6ffd9d563d483420eda2d3f42ff8
Jason Triemert <jaytriemert@gmail.com>
2023-04-18 11:54:42 -0500

    updated files

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-//$(function () {
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
-//});
-
-
 var calendarEntries = JSON.parse(localStorage.getItem('calendarEnties')) || [];
 
 function dateTime() {
@@ -36,7 +11,6 @@ var currTime = dayjs().hour()
 
 for (let i = 9; i < 19; i++) {
   var parentID = document.querySelector("#hour-" + i);
-  console.log(parentID);
   if (i > currTime) {
     parentID.children[1].classList.add("future")
   }
@@ -49,7 +23,6 @@ for (let i = 9; i < 19; i++) {
   parentID.children[1].value = localStorage.getItem("hour-" + i);
   parentID.children[2].addEventListener("click", function () {
     var text = this.previousElementSibling.value
-    // alert(text)
     localStorage.setItem("hour-" + i, text)
   })
 }
